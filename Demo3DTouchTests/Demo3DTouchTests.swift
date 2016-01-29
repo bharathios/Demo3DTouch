d77fcc74cb3b0b304b40773f5853e7d101a03442
Bharath Rao <bharath.rao@olacabs.com>
2016-01-29 22:08:36 +0530

    Initial Commit

---
+++ Demo3DTouchTests/Demo3DTouchTests.swift
@@ -0,0 +1,36 @@
+//
+//  Demo3DTouchTests.swift
+//  Demo3DTouchTests
+//
+//  Created by Bharath Rao on 29/01/16.
+//  Copyright © 2016 Bharath Nagaraj Rao. All rights reserved.
+//
+
+import XCTest
+@testable import Demo3DTouch
+
+class Demo3DTouchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
